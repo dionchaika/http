a096a3df771c75916eeb82c3d58ff963f4704f5f
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-04-17 13:21:49 +0300

    Update CookieStorage.php

--- src/Cookie/CookieStorage.php
+++ src/Cookie/CookieStorage.php
@@ -28,34 +28,4 @@ class CookieStorage
      * @var array
      */
     protected $cookies = [];
-
-    /**
-     * Store cookies from request.
-     *
-     * @param \Psr\Http\Message\RequestInterface $request
-     * @return void
-     */
-    public function storeFromRequest(RequestInterface $request)
-    {
-        foreach ($request->getHeader('Cookie') as $cookie) {
-            try {
-                $this->cookies[] = Cookie::createFromString($cookie);
-            } catch (Exception $e) {}
-        }
-    }
-
-    /**
-     * Store cookies from response.
-     *
-     * @param \Psr\Http\Message\ResponseInterface $response
-     * @return void
-     */
-    public function storeFromResponse(ResponseInterface $response)
-    {
-        foreach ($response->getHeader('Set-Cookie') as $setCookie) {
-            try {
-                $this->cookies[] = Cookie::createFromString($setCookie);
-            } catch (Exception $e) {}
-        }
-    }
 }
